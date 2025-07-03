4bc0cf1a1c32b3dd561791bf0fc9641a019afd2e
blueswayne133 <blueswayne133@gmail.com>
2025-07-03 08:26:29 -0700

    user

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -47,6 +47,18 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" rel="stylesheet" />
 
     @stack('styles')
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'dd5c8d8f757c612fa3e64621ef01c42bb968a1d0';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </head>
 
 <body style="background-color:#000;">
